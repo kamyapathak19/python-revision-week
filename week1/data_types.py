df5541fa0c2267ca66e5ca0478602a4fabe18f25
kamyapathak19 <pkamya2011@gmail.com>
2025-07-27 02:47:54 +0530

    create python files to demonstrate data types

---
+++ week1/data_types.py
@@ -0,0 +1,19 @@
+#data types = it  basically tells us about the type of data stored in variable 
+# like int,float,string,boolean,lists,tuple,dictionary
+
+a = 12
+b = 3.45
+c = "prem is an artist"
+d = True
+e = [1,2,3,4,5]
+f = (12,13,14,15)
+g = {1:"apple", 2:"banana", 3:"orange"}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+
