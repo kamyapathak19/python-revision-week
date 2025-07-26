96b6eba27865a94f29dc8ad2859827236c8f7bc2
kamyapathak19 <pkamya2011@gmail.com>
2025-07-27 05:16:33 +0530

    comparision operators example

---
+++ week1/comparision_operator.py
@@ -0,0 +1,12 @@
+#gives answer in boolean data type 
+ 
+#== (Equal), != (Not Equal), > (Greater Than), < (Less Than), >=
+#(Greater Than or Equal), <= (Less Than or Equal).
+
+print(3 == 3)
+print(3 == 3)
+print(4>5)
+print(4<5)
+print(2>=2)
+print(2<=1)
+
