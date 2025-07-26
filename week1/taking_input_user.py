3f2cd429da220f2a6233ec51f3af7cdaad234504
kamyapathak19 <pkamya2011@gmail.com>
2025-07-27 03:07:20 +0530

    taking input from the user considering different data types

---
+++ week1/taking_input_user.py
@@ -0,0 +1,7 @@
+#here we understang how to take input from the user for different datatypes 
+
+age = int(input("enter your age:"))
+name = input("enter your name:")
+favnum = float(input("enter your favourite decimel number:"))
+
+print(f"hello, my name is {name} and my age is {age}. my favourite decimel number is {favnum}")
