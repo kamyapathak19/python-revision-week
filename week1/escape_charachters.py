11fbdb2445c7616d3a01ff0a0388b9a9afab8d8c
kamyapathak19 <pkamya2011@gmail.com>
2025-07-27 04:53:18 +0530

    using escape characters
    comments
    for printing string in other line we will use \n
    for printing literal \ we use double back slash (\\)
    for printing literal " we use black slash double quotes (\")
    when printing any string if we use comma after double quote a space automatically appearif we give multiple print statement it will automatically print in other line

---
+++ week1/escape_charachters.py
@@ -0,0 +1,13 @@
+#comments 
+'''
+for printing string in other line we will use \n
+for printing literal \ we use double back slash (\\)
+for printing literal " we use black slash double quotes (\")
+when printing any string if we use comma after double quote a space automatically appearif we give multiple print statement it will automatically print in other line  
+
+'''
+print("hey\n how are you?")
+print("hey\\ how are you?")
+print("hey\" how are you?")
+print("hey", "how are you?")
+      
