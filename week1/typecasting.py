de06a3ed0a1646cf5abe6606e20eede3b0350a62
kamyapathak19 <pkamya2011@gmail.com>
2025-07-27 02:58:43 +0530

    here we understand typecasting
    we get to know about different typecasting including int to str , float to int, list to tuple etc

---
+++ week1/typecasting.py
@@ -0,0 +1,14 @@
+#typecasting = it converts one data type to another data type such as str to int or int to float or str to float
+
+a = "23"
+print(int(a))
+
+b = 12.5
+print(int(b))
+
+c = 33
+print(str(c))
+
+d = [1,2,3,4,5]
+print(tuple(d))
+
