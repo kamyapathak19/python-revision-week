4c0b3137646f35449bd7ddd1befd75cbae2aa10a
kamyapathak19 <pkamya2011@gmail.com>
2025-07-27 05:03:03 +0530

    sep and end parameter
    sep and end
    
    as we know after every comma a space appears just like that is we put sept=","
    instead of space a comma will appear at every space present
    
    if we add end="-"
    it will end the sentence with that character and if there is another print statement below that it will be automatically be shifted to the same line.

---
+++ week1/sep_and_end_parameters.py
@@ -0,0 +1,10 @@
+#sep parameter = as we know after every comma a space appears just like that is we put sept=","
+#instead of space a comma will appear at every space present 
+
+print("gugu", "gaga", sep = "+")
+
+#if we add end="-"
+#it will end the sentence with that character and if there is another print statement below that it will be automatically be shifted to the same line.
+
+print("gugu","gaga", sep = "+", end = "=")
+print("gugaguga")
