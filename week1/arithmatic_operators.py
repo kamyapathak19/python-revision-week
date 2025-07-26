92f318abdce2e2d695821144d04efca112575068
kamyapathak19 <pkamya2011@gmail.com>
2025-07-27 05:12:11 +0530

    Create arithmatic_operators.py
    modulus (%)= just gives the remainder
    floor division(//) = just gives the whole number of the quotient

---
+++ week1/arithmatic_operators.py
@@ -0,0 +1,10 @@
+#+(Addition), - (Subtraction), * (Multiplication), / (Division), 
+#%(Modulus), ** (Exponentiation), // (Floor Division).
+
+print(2+3)
+print(4-2)
+print(3*8)
+print(9/3)
+print(5%4) #gives remainders
+print(2**2)
+print(21//2)#just gives the whole numbers
