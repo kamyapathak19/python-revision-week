ddea79536185a28afcb762e26ea40806bd06df73
kamyapathak19 <pkamya2011@gmail.com>
2025-07-27 05:28:19 +0530

    assignment operators example

---
+++ week1/assignment_operators.py
@@ -0,0 +1,31 @@
+
+# =, += , -= , *= , /= , %= , **= , //= .
+
+x = 3
+x += 5
+print (x)
+
+y = 2
+y-= 4
+print(y)
+
+w = 4
+w *= 3
+print(w)
+
+u = 7
+u /= 7
+print(u)
+
+t = 3
+t %= 2
+print(t)
+
+d = 2 
+d **= 2
+print(d)
+
+r = 8 
+r //= 3
+print(r)
+
